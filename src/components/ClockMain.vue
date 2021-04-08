23e5ba488db8f2584db60ab5251f39296360111e
Joyce <yeechiajoyce@gmail.com>
2021-04-08 14:20:45 +0800

    安裝正確版本sass-loader、node-sass

---
+++ src/components/ClockMain.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-container-outer">
+    <div class="main-container-inner">
+			<div class="main-container-inner-box">
+				<clock-title/>
+
+				<clock/>
+
+				<clock-operate/>
+			</div>
+		</div>
+      
+		<p class="product-name">
+			PODOMORO
+		</p>
+  </div>
+</template>
+
+<script>
+import ClockTitle from './ClockTitle';
+import Clock from './Clock';
+import ClockOperate from './ClockOperate';
+
+export default {
+  components: {
+		ClockTitle,
+		Clock,
+		ClockOperate,
+	},
+}
+</script>
