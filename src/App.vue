d0233adcf424e8732469f0427b8776a7715e8f2f
Joyce <yeechiajoyce@gmail.com>
2021-04-08 11:52:18 +0800

    偵測視窗大小，將網頁大小始終維持與視窗等高

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="app"
+    :style="{ height: windowHeight + 'px' }"
+  >
+    <Modal/>
+    <MainArea/>
+    <AsideArea/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal';
+import MainArea from './components/MainArea';
+import AsideArea from './components/AsideArea';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Modal,
+    MainArea,
+    AsideArea,
+  },
+  data() {
+    return {
+      windowHeight: null,
+    };
+  },
+  methods: {
+    getWindowHeight() {
+      this.windowHeight = document.documentElement.clientHeight;
+    },
+  },
+  mounted() {
+    this.getWindowHeight();
+    window.addEventListener('resize', this.getWindowHeight);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.getWindowHeight);
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
 }
 </style>
